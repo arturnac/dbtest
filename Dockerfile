de7bcb0a116d72627a1468a0462fb573a752a670
Ubuntu <ubuntu@ip-172-31-11-240.us-west-2.compute.internal>
2017-05-18 12:58:30 +0000

    update 3

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM python:2.7
+ADD pull.py /
 RUN pip install paho-mqtt
 RUN pip install pymongo
 
-CMD ["python", "pull.py"]
+CMD ["python", "./pull.py"]
--- build_and_run.sh
+++ build_and_run.sh
@@ -2,7 +2,7 @@
 
 docker pull mongo:3.2
 docker rm -f mongo
-docker run --name mongo -v mongo:/data/db -d mongo
+docker run --name mongo -v mongo:/data/db -d -p 27017:27017 mongo --noauth --bind_ip=0.0.0.0  
 
 docker build -t pull .
 docker rm -f pull
